5f303fc73c25fd5d204a77b64d9e413f3bb69dd7
Romain Chebrek <rchebrek@bi-consulting.com>
2023-08-11 11:46:43 +0200

    add rest call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import requests
 
 ####################
 
@@ -28,3 +29,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 ####################
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
