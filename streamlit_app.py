60e42c7184b9328b2f4c47ddd232c03e9bf56597
Romain Chebrek <rchebrek@bi-consulting.com>
2023-08-11 13:08:13 +0200

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,12 +33,13 @@ streamlit.dataframe(fruits_to_show)
 
 ####################
 
-def get_fruityvice_data(fruit_choice):
-    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruityvice_data(fc):
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fc}")
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
 try :
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     streamlit.write('The user entered ', fruit_choice)
@@ -52,17 +53,24 @@ except URLError as e :
 
 ####################
 
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("Fruit load list contains :")
-streamlit.dataframe(my_data_row)
+
+
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return(my_cur.fetchall())
+
+def insert_fruit(ft):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('{ft}')")
+    
+if streamlit.button('Get fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.dataframe(get_fruit_load_list)
 
 fruit_toadd = streamlit.text_input('What fruit would you like to add ?')
 streamlit.write('The user added ', fruit_toadd)
 
-if fruit_toadd := '' :
-    my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('{fruit_toadd}')")
+if streamlit.button('Add fruit to load list') and fruit_toadd != '':
+    insert_fruit(fruit_toadd)
